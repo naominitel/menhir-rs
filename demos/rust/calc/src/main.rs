261651e4159985203aa9109677de5316802a11e3
Naomi Nitel <naominitel@gmail.com>
2016-05-29 13:21:48 +0200

    Add tests for Rust examples.

--- demos/rust/calc/src/main.rs
+++ demos/rust/calc/src/main.rs
@@ -3,10 +3,17 @@ mod parser { include!(concat!(env!("OUT_DIR"), "/parser.rs")); }
 
 fn main() {
     use parser::Token::*;
-    let input = vec![INT(5), TIMES, INT(3), MINUS, OP, INT(2), PLUS, INT(4), CL, MINUS, INT(2), EOL];
+    let mut input = vec![
+	INT(5), TIMES, INT(3), MINUS, OP, INT(2),
+	 PLUS, INT(4), CL, MINUS, INT(2), EOL
+    ].into_iter();
     env_logger::init().unwrap();
-    match parser::main(&mut input.into_iter()) {
+    match parser::main(&mut input) {
         Ok(res) => println!("res = {}", res),
-	Err(_)  => println!("syntax error")
+	Err(_)  => panic!("syntax error")
     }
+    assert!(if let None = input.next() { true } else { false });
 }
+
+#[test]
+fn test() { main() }
--- demos/rust/lambda/src/main.rs
+++ demos/rust/lambda/src/main.rs
@@ -3,17 +3,21 @@ mod parser { include!(concat!(env!("OUT_DIR"), "/parser.rs")); }
 
 fn main() {
     use parser::Token::*;
-    let input = vec![
+    let mut input = vec![
         OP, LAMBDA, ID(format!("x")), DOT, ID(format!("x")), CL,
         OP, LAMBDA, ID(format!("x")), DOT, LAMBDA, ID(format!("y")), DOT,
             ID(format!("x")), OP, LAMBDA, ID(format!("x")), DOT,
                                   ID(format!("x")), ID(format!("y")), CL,
         CL,
         EOF
-    ];
+    ].into_iter();
     env_logger::init().unwrap();
-    match parser::main(&mut input.into_iter()) {
+    match parser::main(&mut input) {
         Ok(res) => println!("res = {:?}", res),
-        Err(_)  => println!("syntax error")
+        Err(_)  => panic!("syntax error")
     }
+    assert!(if let None = input.next() { true } else { false });
 }
+
+#[test]
+fn test() { main() }
