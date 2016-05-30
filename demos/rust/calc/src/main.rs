f9e865d2d5149c13a1d67c2abbbf444ebd1ee014
Naomi Nitel <naominitel@gmail.com>
2016-05-30 17:33:21 +0200

    Add fail tests.

--- demos/rust/calc/src/main.rs
+++ demos/rust/calc/src/main.rs
@@ -1,18 +1,35 @@
 extern crate menhir_runtime;
 mod parser { include!(concat!(env!("OUT_DIR"), "/parser.rs")); }
 
+use menhir_runtime::*;
+
+macro_rules! run(
+    ($tokens:expr) => {
+        parser::main(&mut IteratorLexer::new(&mut $tokens.enumerate()))
+    }
+);
+
 fn main() {
     use parser::Token::*;
+
     let mut input = vec![
         INT(5), TIMES, INT(3), MINUS, OP, INT(2),
         PLUS, INT(4), CL, MINUS, INT(2), EOL
-    ].into_iter().enumerate();
-    match parser::main(&mut menhir_runtime::IteratorLexer::new(&mut input)) {
-        Ok(res) => println!("res = {}", res),
-        Err(menhir_runtime::ParserError::SyntaxError(pos)) => panic!("syntax error at {}", pos),
-        Err(menhir_runtime::ParserError::LexerError(err)) => panic!("lexer error: {:?}", err)
+    ].into_iter();
+    match run!(&mut input) {
+        Ok(res) => println!("res = {:?}", res),
+        Err(ParserError::SyntaxError(pos)) => panic!("syntax error at {}", pos),
+        Err(ParserError::LexerError(err)) => panic!("lexer error: {:?}", err)
     }
     assert!(if let None = input.next() { true } else { false });
+
+    let input = vec![TIMES];
+    assert!(if let Err(ParserError::SyntaxError(0)) =
+            run!(&mut input.into_iter()) { true } else { false });
+
+    let input: Vec<parser::Token> = vec![];
+    assert!(if let Err(ParserError::LexerError(UnexpectedEof(0))) =
+            run!(&mut input.into_iter()) { true } else { false });
 }
 
 #[test]
--- demos/rust/lambda/src/main.rs
+++ demos/rust/lambda/src/main.rs
@@ -1,8 +1,17 @@
 extern crate menhir_runtime;
 mod parser { include!(concat!(env!("OUT_DIR"), "/parser.rs")); }
 
+use menhir_runtime::*;
+
+macro_rules! run(
+    ($tokens:expr) => {
+        parser::main(&mut IteratorLexer::new(&mut $tokens.enumerate()))
+    }
+);
+
 fn main() {
     use parser::Token::*;
+
     let mut input = vec![
         OP, LAMBDA, ID(format!("x")), DOT, ID(format!("x")), CL,
         OP, LAMBDA, ID(format!("x")), DOT, LAMBDA, ID(format!("y")), DOT,
@@ -10,13 +19,21 @@ fn main() {
                                   ID(format!("x")), ID(format!("y")), CL,
         CL,
         EOF
-    ].into_iter().enumerate();
-    match parser::main(&mut menhir_runtime::IteratorLexer::new(&mut input)) {
+    ].into_iter();
+    match run!(&mut input) {
         Ok(res) => println!("res = {:?}", res),
-        Err(menhir_runtime::ParserError::SyntaxError(pos)) => panic!("syntax error at {}", pos),
-        Err(menhir_runtime::ParserError::LexerError(err)) => panic!("lexer error: {:?}", err)
+        Err(ParserError::SyntaxError(pos)) => panic!("syntax error at {}", pos),
+        Err(ParserError::LexerError(err)) => panic!("lexer error: {:?}", err)
     }
     assert!(if let None = input.next() { true } else { false });
+
+    let input = vec![CL];
+    assert!(if let Err(ParserError::SyntaxError(0)) =
+            run!(&mut input.into_iter()) { true } else { false });
+
+    let input: Vec<parser::Token> = vec![];
+    assert!(if let Err(ParserError::LexerError(UnexpectedEof(0))) =
+            run!(&mut input.into_iter()) { true } else { false });
 }
 
 #[test]
